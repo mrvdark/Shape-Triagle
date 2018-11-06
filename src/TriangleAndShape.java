00bc93b50177264069f250763b1d2e995ef15aca
mrvdark <m.r.v.leo.dark@gmail.com>
2018-11-06 11:57:20 +0700

    done

---
+++ src/TriangleAndShape.java
@@ -0,0 +1,10 @@
+
+
+public class TriangleAndShape {
+
+    public static void main(String[] args) {
+        Triangle Delta = new Triangle(3, 4, 5);
+        System.out.println(Delta.toString());
+    }
+
+}
